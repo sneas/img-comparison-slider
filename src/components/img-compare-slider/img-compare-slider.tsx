697adffc9e327da6015552b212a3604bc8d84abb
Dima Snisarenko <snisarenkodima@gmail.com>
2019-08-03 18:04:53 +0200

    feat: apply after width on slider move

--- src/components/img-compare-slider/img-compare-slider.tsx
+++ src/components/img-compare-slider/img-compare-slider.tsx
@@ -6,13 +6,26 @@ import { Component, h } from '@stencil/core';
   shadow: true
 })
 export class ImgCompareSlider {
+  private after?: HTMLElement;
+  private slider?: HTMLInputElement;
+
+  componentDidRender() {
+    const redrawImage  = () => {
+      this.after.style.width = `${this.slider.value}%`;
+    };
+
+    this.slider.addEventListener('input', redrawImage);
+    this.slider.addEventListener('change', redrawImage);
+    redrawImage();
+  }
+
   render() {
     return <div>
       <slot name="before"></slot>
-      <div class="after">
+      <div ref={el => this.after = el as HTMLElement} class="after">
         <slot name="after"></slot>
       </div>
-      <input id="slider" type="range" min="0" max="100" step="0.5" />
+      <input ref={el => this.slider = el as HTMLInputElement} id="slider" type="range" min="0" max="100" step="0.5" />
     </div>;
   }
 }
