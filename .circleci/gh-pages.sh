24cd4329d1fc3d8dd91421dbae99401ef3b7aed2
Dima Snisarenko <snisarenkodima@gmail.com>
2019-08-10 14:50:49 +0200

    chore(ci): automatically publish new version to NPM and gh-pages

---
+++ .circleci/gh-pages.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ -z "$GH_NAME" ] || [ -z "$GH_EMAIL" ]; then
+ echo "GH_NAME and GH_EMAIL variables must be set"
+ exit 1
+fi
+
+git config --global user.name "$GH_NAME"
+git config --global user.email "$GH_EMAIL"
+
+npm run gh -- -m "Automatically update gh-pages [skip ci]"
