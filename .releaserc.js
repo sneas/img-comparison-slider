822319131df9d5e7d00c189282603f145989ce60
Dima Snisarenko <snisarenkodima@gmail.com>
2020-11-01 11:27:48 +0100

    chore(release): automatically generate release notes

--- .releaserc.js
+++ .releaserc.js
@@ -2,6 +2,16 @@ module.exports = {
   ci: true,
   branches: ['master'],
   plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
+        },
+      },
+    ],
+    '@semantic-release/release-notes-generator',
     '@semantic-release/npm',
     '@semantic-release/github',
     [
