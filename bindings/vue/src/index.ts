5d201518be32307d81e8d51028b51c760aca0956
Dima Snisarenko <snisarenkodima@gmail.com>
2021-06-03 18:13:23 +0200

    bindings(vue): utilize vanila component
    fix #25

--- bindings/vue/src/index.ts
+++ bindings/vue/src/index.ts
@@ -1,1 +1,9 @@
-export * from './components';
+import { defineComponent, h } from 'vue';
+import 'img-comparison-slider';
+
+export const ImgComparisonSlider = defineComponent({
+  name: 'ImgComparisonSlider',
+  setup(props, { slots }) {
+    return () => h('img-comparison-slider', slots.default());
+  },
+});
