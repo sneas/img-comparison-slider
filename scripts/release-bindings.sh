d7d828df52ebade0db344a10a9ab38a36deef2ca
Dima Snisarenko <snisarenkodima@gmail.com>
2021-06-05 09:44:19 +0200

    ci(bindings): authenticate with NPM_TOKEN

--- scripts/release-bindings.sh
+++ scripts/release-bindings.sh
@@ -4,6 +4,7 @@ ROOT=$PWD
 
 for project in bindings/*/; do
     cd $project
+    echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" >> .npmrc
     npm install
     npm i -S $npm_package_name@$npm_package_version
     npm run build
