e023ebe3a4974aeca838dc998576dac5145447f0
Dima Snisarenko <snisarenkodima@gmail.com>
2021-06-18 10:18:39 +0200

    fix(react): ssr
    fix #32

--- bindings/react/src/index.tsx
+++ bindings/react/src/index.tsx
@@ -1,14 +1,18 @@
 import React, { FC, AllHTMLAttributes, PropsWithChildren } from 'react';
-import 'img-comparison-slider';
+
+if (typeof document !== 'undefined') {
+  import('img-comparison-slider');
+}
 
 type ImgComparisonSliderProps = AllHTMLAttributes<HTMLElement>;
 
 export const ImgComparisonSlider: FC<ImgComparisonSliderProps> = ({
   children,
   ...props
-}: PropsWithChildren<ImgComparisonSliderProps>) =>
-  React.createElement(
+}: PropsWithChildren<ImgComparisonSliderProps>) => {
+  return React.createElement(
     'img-comparison-slider',
     Object.assign({}, props),
     children
   );
+};
