44c68278b7231c6a2274fc5c27c7baabc2172ec0
Dmytro Snisarenko <snisarenkodima@gmail.com>
2021-07-13 20:05:13 +0300

    demo: landing page (#44)
    * refactor(demo): use handlebars to render demo
    
    * chore(dev-server): configure host with environment variables
    
    * refactor(handle): get rid of svg width and path stroke color
    
    * demo: index page with jumbotron
    
    * demo: features
    
    * demo: image width 2560px
    
    * demo: footer
    
    * demo(index): shadow opacity .28
    
    * demo(examples): 'usage examples' header
    
    * demo: base url
    
    * demo: remove gh button script
    
    * demo(index): shrink image

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const { merge } = require('webpack-merge');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const indexParameters = require('./public/templateParameters/index');
 
 const commonConfig = {
   mode: 'production',
@@ -46,6 +47,13 @@ const commonConfig = {
           esModule: false,
         },
       },
+      {
+        test: /\.hbs$/,
+        loader: 'handlebars-loader',
+        options: {
+          query: { inlineRequires: '/public/' },
+        },
+      },
     ],
   },
   resolve: {
@@ -63,26 +71,45 @@ const commonConfig = {
   ],
 };
 
-const demoConfig = ({ favicon = 'public/favicon.svg' } = {}) => ({
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: 'index.html',
-      inject: true,
-      favicon,
-    }),
-    new CopyWebpackPlugin({
-      patterns: [{ from: 'public/static', to: '' }],
-    }),
-  ],
-  output: {
-    path: path.resolve(__dirname, 'demo'),
-  },
-  devServer: {
-    host: process.env.HOST ?? '0.0.0.0',
-    useLocalIp: process.env.NO_LOCAL_IP !== 'true',
-  },
-});
+const demoConfig = ({ favicon = 'public/favicon.svg', base = '/' } = {}) => {
+  const htmlOptions = {
+    inject: true,
+    favicon,
+    base,
+  };
+
+  return {
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.hbs',
+        filename: 'index.html',
+        templateParameters: indexParameters,
+        ...htmlOptions,
+      }),
+      new HtmlWebpackPlugin({
+        template: './public/examples.hbs',
+        filename: 'examples.html',
+        ...htmlOptions,
+      }),
+      new CopyWebpackPlugin({
+        patterns: [{ from: 'public/static', to: '' }],
+      }),
+      new CopyWebpackPlugin({
+        patterns: [{ from: 'public/images', to: 'images' }],
+      }),
+      new CopyWebpackPlugin({
+        patterns: [{ from: 'public/main.css', to: 'main.css' }],
+      }),
+    ],
+    output: {
+      path: path.resolve(__dirname, 'demo'),
+    },
+    devServer: {
+      host: process.env.HOST ?? '0.0.0.0',
+      useLocalIp: process.env.NO_LOCAL_IP !== 'true',
+    },
+  };
+};
 
 const devConfig = {
   mode: 'development',
@@ -99,7 +126,12 @@ module.exports = (env) => {
         devConfig
       );
     case env.demo:
-      return merge(commonConfig, demoConfig());
+      return merge(
+        commonConfig,
+        demoConfig({
+          base: '/img-comparison-slider/',
+        })
+      );
     case env.production:
       return merge(commonConfig, prodConfig);
     default:
