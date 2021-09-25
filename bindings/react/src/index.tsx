6a75a79ee36f521aa083a3826a197845a32a24de
Dima Snisarenko <snisarenkodima@gmail.com>
2021-09-25 13:24:11 +0200

    feat: trigger slide event
    fix #53, re #50

--- bindings/react/src/index.tsx
+++ bindings/react/src/index.tsx
@@ -1,24 +1,42 @@
-import React, { FC, AllHTMLAttributes, PropsWithChildren } from 'react';
+import React, {
+  FC,
+  AllHTMLAttributes,
+  PropsWithChildren,
+  ChangeEventHandler,
+} from 'react';
 
 if (typeof document !== 'undefined') {
   import('img-comparison-slider');
 }
 
-type ImgComparisonSliderProps = AllHTMLAttributes<HTMLElement> & {
-  value?: number | string;
-  hover?: boolean;
+type HTMLImgComparisonSliderElement = HTMLElement & {
+  value: number;
+  hover: boolean;
 };
+
+type ImgComparisonSliderProps =
+  AllHTMLAttributes<HTMLImgComparisonSliderElement> & {
+    value?: number | string;
+    hover?: boolean;
+    onSlide?: ChangeEventHandler<HTMLImgComparisonSliderElement>;
+  };
 
 export const ImgComparisonSlider: FC<ImgComparisonSliderProps> = ({
   children,
+  onSlide = () => null,
   ...props
 }: PropsWithChildren<ImgComparisonSliderProps>) => {
-  const ref = React.createRef<ImgComparisonSliderProps>();
+  const ref = React.createRef<HTMLImgComparisonSliderElement>();
   React.useEffect(() => {
     if (props.value !== undefined) {
-      ref.current.value = props.value;
+      ref.current.value = parseFloat(props.value.toString());
     }
   }, [props.value, ref]);
+
+  React.useEffect(() => {
+    // @ts-ignore
+    ref.current.addEventListener('slide', onSlide);
+  }, [ref]);
 
   return React.createElement(
     'img-comparison-slider',
