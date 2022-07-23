37f0dd8d67bc8b69ffbbfb9fcbcaae8c08946f79
Dima Snisarenko <snisarenkodima@gmail.com>
2022-07-23 12:09:12 +0200

    refactor: move main package and demo under monorepo

---
+++ packages/img-comparison-slider/webpack.config.js
@@ -0,0 +1,63 @@
+const { merge } = require('webpack-merge');
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const prodConfig = {
+  mode: 'production',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: false,
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /src\/.+\.html/,
+        loader: 'html-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'source-map',
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: 'src/assets', to: '' }],
+    }),
+  ],
+};
+
+const devConfig = {
+  mode: 'development',
+};
+
+module.exports = (env) => {
+  switch (true) {
+    case env.development:
+      return merge(prodConfig, devConfig);
+    case env.production:
+      return prodConfig;
+    default:
+      throw new Error('No matching configuration was found!');
+  }
+};
---
+++ packages/www/webpack.config.js
@@ -0,0 +1,108 @@
+const { merge } = require('webpack-merge');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const indexParameters = require('./src/templateParameters/index');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const config = ({ favicon = 'src/favicon.svg' } = {}) => {
+  const htmlOptions = {
+    inject: true,
+    favicon,
+  };
+
+  return {
+    mode: 'production',
+    entry: './src/index.ts',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?[ac]ss$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'resolve-url-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(webp|svg)$/i,
+          loader: 'file-loader',
+          options: {
+            name: 'images/[name].[ext]',
+            esModule: false,
+          },
+        },
+        {
+          test: /favicon\.svg?$/,
+          loader: 'file-loader',
+          options: {
+            esModule: false,
+          },
+        },
+        {
+          test: /\.hbs$/,
+          loader: 'handlebars-loader',
+          options: {
+            query: { inlineRequires: '/src/' },
+          },
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css',
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/index.hbs',
+        filename: 'index.html',
+        templateParameters: indexParameters,
+        ...htmlOptions,
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/examples.hbs',
+        filename: 'examples.html',
+        ...htmlOptions,
+      }),
+      new CopyWebpackPlugin({
+        patterns: [{ from: 'src/static', to: '' }],
+      }),
+      new CopyWebpackPlugin({
+        patterns: [{ from: 'src/images', to: 'images' }],
+      }),
+    ],
+    output: {
+      chunkFilename: '[name].[contenthash].js',
+      filename: '[name].[contenthash].js',
+      assetModuleFilename: '[name].[contenthash][ext][query]',
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js', '.svg'],
+    },
+    devtool: 'source-map',
+    devServer: {
+      host: process.env.HOST ?? '0.0.0.0',
+      useLocalIp: process.env.NO_LOCAL_IP !== 'true',
+    },
+  };
+};
+
+const devConfig = {
+  mode: 'development',
+};
+
+module.exports = (env) => {
+  switch (true) {
+    case env.development:
+      return merge(config({ favicon: 'src/favicon-dev.svg' }), devConfig);
+    case env.production:
+      return config();
+    default:
+      throw new Error('No matching configuration was found!');
+  }
+};
